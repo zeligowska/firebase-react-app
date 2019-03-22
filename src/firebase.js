826300ae0e1017fb23b25e92a9a865f19646903c
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2019-03-22 21:26:21 +0100

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebase = require('firebase');
+
+var config = {
+    apiKey: "AIzaSyBJvyOwZtz41vVh4YX437xpzOcL1gjKal0",
+    authDomain: "fir-react-app-8a10c.firebaseapp.com",
+    databaseURL: "https://fir-react-app-8a10c.firebaseio.com",
+    projectId: "fir-react-app-8a10c",
+    storageBucket: "fir-react-app-8a10c.appspot.com",
+    messagingSenderId: "825432621986"
+  };
+  firebase.initializeApp(config);
